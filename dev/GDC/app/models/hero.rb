1a2fa83fa94f2c78e4dc2ab5dfd0b6bae9bcfbef
Fabio Maiolo <fabio_maiolo@hotmail.com>
2020-09-06 14:27:13 -0300

    created some views and controllers for heroes

--- dev/GDC/app/models/hero.rb
+++ dev/GDC/app/models/hero.rb
@@ -1,6 +1,7 @@
 class Hero < ApplicationRecord
   self.table_name = "heroes"
+  has_one_attached :photo
 
   validates :hero_name, uniqueness: true
-  validates :hero_class, inclusion: { in: ["S","A","B","C"] }
+  validates :hero_class, inclusion: { in: ['S','A','B','C'] }
 end
